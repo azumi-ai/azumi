d64801f9b46e045ddde287ac11e4974715b6bcb3
team-azumi <n5x2l4x476dvntz0@outlook.com>
2025-01-16 06:32:29 +0300

    Create server.py

---
+++ api/websocket/server.py
@@ -0,0 +1,32 @@
+from fastapi import WebSocket, WebSocketDisconnect
+from typing import Dict, List, Any
+import json
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[str, WebSocket] = {}
+        
+    async def connect(self, websocket: WebSocket, client_id: str):
+        await websocket.accept()
+        self.active_connections[client_id] = websocket
+        
+    def disconnect(self, client_id: str):
+        if client_id in self.active_connections:
+            del self.active_connections[client_id]
+            
+    async def send_message(self, message: str, client_id: str):
+        if client_id in self.active_connections:
+            await self.active_connections[client_id].send_text(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket, client_id)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            response = process_websocket_message(data)
+            await manager.send_message(json.dumps(response), client_id)
+    except WebSocketDisconnect:
+        manager.disconnect(client_id)
